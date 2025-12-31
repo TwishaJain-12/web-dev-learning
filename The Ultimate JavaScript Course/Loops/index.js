c53df2612c0b7bde5fd501e0b13177aae4d98812
TwishaJain-12 <twisha122124@gmail.com>
2025-12-31 17:56:57 +0530

    JS Loops

---
+++ The Ultimate JavaScript Course/Loops/index.js
@@ -0,0 +1,47 @@
+console.log("JS Loops")
+
+//for ; for in ; for of ; while ; do-while ;
+
+// for(let i=1; i<26; i++){
+//     console.log(i)
+// }
+
+
+// let obj = {
+//     name: "Twisha" , 
+//     role: "Programmer",
+//     company: "CodeWithTwiss"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element)
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key)
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(element)
+// }
+
+
+// for (const c of "Twisha") {
+//     console.log(c)
+// }
+
+
+// let i = 4;
+// while(i<10){
+//     console.log(i)
+//     i++
+// }
+
+
+let j = 4;
+do{
+    console.log(j)
+    j++
+}
+while(j<3)
