7281016abdce58116cf2eda96feed7a791b8778b
TwishaJain-12 <twisha122124@gmail.com>
2026-01-06 00:00:00 +0530

    Arr

--- The Ultimate JavaScript Course/Arrays/mfr.js
+++ The Ultimate JavaScript Course/Arrays/mfr.js
@@ -7,3 +7,26 @@ let arr = [1,13,5,7,11]
 // }
 // console.log(newArr)
 
+let newArr = arr.map(e=>{ //in map we can take value, index, array
+    return e**2
+})
+
+console.log(newArr)
+
+//FILTER
+const greaterThanSeven = (e)=>{
+    return e>7 ? true:false
+}
+console.log(arr.filter(greaterThanSeven)) //returns the new array
+
+
+
+//REDUCE 
+let Arr = [1,2,3,4,5,6]
+
+const red = (a,b)=>{
+    return a*b
+}
+
+console.log(Arr.reduce(red)) //red func applied to all its elements --> obtains a value
+
